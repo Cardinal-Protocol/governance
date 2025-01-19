32dd2569f3dc4ee89af0e644020acfdbb57ea44c
Your Name <you@example.com>
2025-01-19 18:14:22 -0500

    Updated

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,11 @@ import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-solhint";
+import "hardhat-contract-sizer";
+
+
+const accounts: string[] = [`0x${process.env.PRIVATE_KEY}`];
+const oneGwei: number = 1000000000;
 
 export default {
 	etherscan: {
@@ -30,47 +35,57 @@ export default {
 	networks: {
 		goerli: {
 			url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
+			accounts
 		},
 		ropsten: {
 			url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
+			accounts
 		},
 		mainnet: {
 			url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
+			accounts
 		},
 		optimism: {
 			url: `https://mainnet.optimism.io`,
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
+			accounts,
 			gasPrice: 15000000,
 			ovm: true
 		},
 		optimismgoerli: {
 			url: `https://goerli.optimism.io`,
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
+			accounts,
 			gasPrice: 15000000
 		},
 		sepolia: {
 			url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string]
+			accounts,
+			chainId: 11155111,
+			gasPrice: oneGwei,
 		},
-		// for mainnet
 		'base-mainnet': {
 			url: 'https://mainnet.base.org',
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
-			gasPrice: 1000000000,
+			accounts,
+			gasPrice: oneGwei,
 		},
-		  // for testnet
 		'base-sepolia': {
 			url: 'https://sepolia.base.org',
-			accounts: [`0x${process.env.PRIVATE_KEY}` as string],
-			gasPrice: 1000000000,
+			accounts,
 			chainId: 84532,
-		}
+			gasPrice: oneGwei,
+		},
+		hardhat: {
+			allowUnlimitedContractSize: true,
+		},
 	},
 	paths: {
 		sources: "./contracts",
 	},
-	solidity: "0.8.18",
+	solidity: "0.8.19",
+
+	settings: {
+		optimizer: {
+			enabled: true,
+			runs: 500,
+		},
+	}
 } as HardhatUserConfig;
